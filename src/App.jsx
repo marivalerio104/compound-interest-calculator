acd9c5cb3e606cd9f1cd9eaa306bbd37ed6c5f50
Mariela Valerio <139282125+marivalerio104@users.noreply.github.com>
2026-02-17 10:54:10 -0600

    Add user input state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import UserInput from './components/UserInput/UserInput'
+import ResultsTable from './components/ResultsTable/ResultsTable'
 import './App.css'
 
 export default function App() {
+  const [userInput, setUserInput] = useState();
+
   return (<>
-    <UserInput />
+    <UserInput setUserInput={setUserInput} />
+    {userInput && <ResultsTable userInput={userInput} />}
   </>)
 }
--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,22 +1,48 @@
+import { useState } from 'react'
 import './UserInput.css'
 
-export default function UserInput() {
-  return <form id="user-input">
-    <div className="input-group">
+export default function UserInput({ setUserInput }) {
+  const [input, setInput] = useState({
+    initial: 1000,
+    annual: 1200,
+    interest: 6,
+    years: 10
+  });
+
+  function handleChange(event) {
+    const { id, value } = event.target;
+    setInput(prev => ({ ...prev, [id]: (value === "" ? "" : Number(value)) }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setUserInput(input);
+  }
+
+  return <form id="user-input" onSubmit={handleSubmit}>
+    <div>
       <label htmlFor="initial-input">INITIAL INVESTMENT</label>
-      <input type="number" id="initial-input" />
+      <input type="number" id="initial" value={input.initial} required
+        onChange={handleChange}
+      />
     </div>
-    <div className="input-group">
-      <label htmlFor="annual-input">ANNUAL CONTRIBUTION</label>
-      <input type="number" id="annual-input" />
+    <div>
+      <label htmlFor="contribution-input">ANNUAL CONTRIBUTION</label>
+      <input type="number" id="annual" value={input.annual} required
+        onChange={handleChange}
+      />
     </div>
-    <div className="input-group">
+    <div>
       <label htmlFor="interest-input">INTEREST RATE</label>
-      <input type="number" id="interest-input" />
+      <input type="number" id="interest" value={input.interest} required
+        onChange={handleChange}
+      />
     </div>
-    <div className="input-group">
+    <div>
       <label htmlFor="years-input">YEARS</label>
-      <input type="number" id="years-input" />
+      <input type="number" id="years" value={input.years} required
+        onChange={handleChange}
+      />
     </div>
     <button>Calculate</button>
   </form>
